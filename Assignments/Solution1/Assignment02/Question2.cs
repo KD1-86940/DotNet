2fbccde35d2f3ace4087df7e724406cda0f6a73f
parag chaudhari <chaudhariparag634@gamil.com>
2024-12-28 14:34:11 +0530

    Assignment02

---
+++ Assignments/Solution1/Assignment02/Question2.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment02
+{
+    internal class Question2
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("How Many records you want to insert : ");
+            int size=Convert.ToInt32(Console.ReadLine());   
+            Assignment02.Student[] students = new Student[size];
+            CreateArray(students);
+            AcceptInfo(students);
+            PrintInfo(students);
+            ReverseArray(students);
+            PrintInfo(students);
+        }
+
+        private static void ReverseArray(Student[] students)
+        {
+            for(int i = 0,j=students.Length-1;i<j;i++,j--)
+            {
+                Student temp = students[i];
+                students[i] = students[j];
+                students[j] = temp;
+            }
+        }
+
+        private static void PrintInfo(Student[] students)
+        {
+            Console.WriteLine("Students Details : ");
+            for(int i = 0; i < students.Length; i++)
+            {
+                Console.WriteLine($"Student [{i + 1}] : ");
+                students[i].PrintDetails(); 
+            }
+        }
+
+        private static void AcceptInfo(Student[] students)
+        {
+            for (int i = 0;i < students.Length;i++)
+            {
+                students[i].AcceptDetails();
+            }
+        }
+
+        private static void CreateArray(Student[] students)
+        {
+            for (int i = 0; i < students.Length; i++)
+            {
+                students[i] = new Student();    
+            }
+        }
+    }
+}
---
+++ Assignments/Solution1/Assignment02/Student.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment02
+{
+    internal struct Student
+    {
+        private String name;
+        private bool gender;
+        private int age;
+        private int std;
+        private char div;
+        private double marks;
+
+        public void InitializeDefaults()
+        {
+            name = string.Empty;
+            gender = false;
+            age = 0;
+            std = 0;
+            div = 'A';
+            marks = 0.0;
+        }
+
+        public Student(String name,bool gender , int age,int std,char div,double marks)
+        {
+            this.name =name;
+            this.gender = gender;
+            this.age = age;
+            this.std = std;
+            this.div = div;
+            this.marks = marks;
+        }
+         
+        public String Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public bool Gender
+        {
+            get { return gender; }
+            set { gender = value; }
+        }
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+        public int Std
+        {
+            get { return std; }
+            set { std = value; }
+        }
+        public char Div
+        {
+            get { return div; }
+            set { div = value; }
+        }
+        public double Marks
+        {
+            get { return marks; }
+            set { marks = value; }  
+        }
+        public void AcceptDetails()
+        {
+            Console.WriteLine("Enter Student Details : ");
+            Console.WriteLine("Name     : ");
+            name=Console.ReadLine();
+            Console.WriteLine("Gender   : ");
+            gender=Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("Age      : ");
+            age=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Standard : ");
+            std=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Division : ");
+            div = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Marks    : ");
+            marks=Convert.ToDouble(Console.ReadLine());
+
+        }
+        public void PrintDetails()
+        {
+            Console.WriteLine($"Name     : { name}");
+            Console.WriteLine($"Gender   : {(gender ? "male" : "female")}");
+            Console.WriteLine($"Age      : { age}");
+            Console.WriteLine($"Standard : {std}");
+            Console.WriteLine($"Division : {div}");
+            Console.WriteLine($"Marks    : {marks}");
+        }
+        /*static void Main(string[] args)
+        {
+            Student s = new Student();
+            s.AcceptDetails();
+            s.PrintDetails();
+        }*/
+    }
+}
